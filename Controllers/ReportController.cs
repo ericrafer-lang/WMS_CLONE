f1d0df65b51baec9aa318d7512db3e12dbc833fd
Axcel Guevarra <axcelbattleground@gmail.com>
2026-07-14 22:51:18 +0800

    created Controllers after converting RazorPages to MVC (VIEWS)

---
+++ Controllers/ReportController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace practice_for_wms.Controllers
+{
+    public class ReportController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
