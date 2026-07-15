9a34ebdf02d91814a7178b5416481223af879c34
Axcel Guevarra <axcelbattleground@gmail.com>
2026-07-15 18:17:14 +0800

    draft models

---
+++ Models/Users.cs
@@ -0,0 +1,15 @@
+﻿namespace practice_for_wms.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+        public int BranchId { get; set; }
+        public bool isActive { get; set; }
+
+    }
+}
