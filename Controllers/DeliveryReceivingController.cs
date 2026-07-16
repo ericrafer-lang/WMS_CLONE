e543255a949798f13334116ffc60e44b8bc6fb8a
Clarence0404 <clarencejames.mendoza@citycollegeoftagaytay.edu.ph>
2026-07-16 08:26:08 +0800

    Add Delivery Receiving and My Task for the Staff

---
+++ Controllers/DeliveryReceivingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace practice_for_wms.Controllers
+{
+    public class DeliveryReceivingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Views/DeliveryReceiving/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace practice_for_wms.Views.DeliveryReceiving
+{
+    public class DeliveryReceivingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
