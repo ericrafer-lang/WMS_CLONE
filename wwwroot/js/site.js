d1852917a567813e5ecb942c8445f8fe817ce7a5
Axcel Guevarra <axcelbattleground@gmail.com>
2026-07-16 16:35:12 +0800

    feat: create reusable function for modals

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,30 +2,37 @@
 // for details on configuring this project to bundle and minify static web assets.
 
     // MODAL SECTION
-function initializeModal(modalId, openId, closeId, cancelId) {
-    const modal = document.getElementById(modalId);
-    const openBtn = document.getElementById(openId);
-    const closeBtn = document.getElementById(closeId);
-    const cancelBtn = document.getElementById(cancelId);
+function initializeModals() {
+    // open modal
+    document.querySelectorAll(".open-modal").forEach((button) => {
+        button.addEventListener("click", () => {
+            const modalId = button.dataset.modal;
+            const modal = document.getElementById(modalId);
 
-    if (!modal || !openBtn || !closeBtn || !cancelBtn) return;
-
-    function closeModal() {
-        modal.classList.remove("flex");
-        modal.classList.add("hidden");
-    }
-
-    openBtn.addEventListener("click", () => {
-        modal.classList.remove("hidden");
-        modal.classList.add("flex");
+            modal.classList.remove("hidden");
+            modal.classList.add("flex");
+        });
     });
 
-    closeBtn.addEventListener("click", closeModal);
-    cancelBtn.addEventListener("click", closeModal);
+    // close modal
+    document.querySelectorAll(".close-modal").forEach((button) => {
+        button.addEventListener("click", () => {
+            const modal = button.closest(".modal-overlay");
 
-    modal.addEventListener("click", (e) => {
-        if (e.target === modal) {
-            closeModal();
-        }
+            modal.classList.remove("flex");
+            modal.classList.add("hidden");
+        });
     });
+
+
+    document.querySelectorAll(".modal-overlay").forEach((modal) => {
+        modal.addEventListener("click", (e) => {
+            if (e.target === modal) {
+                modal.classList.remove("flex");
+                modal.classList.add("hidden");
+            }
+        })
+    })
 }
+
+initializeModals();
