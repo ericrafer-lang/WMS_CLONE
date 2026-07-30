1276960d69d9ada6d518ebb32dc4ad071d2eda1c
Axcel Guevarra <axcelbattleground@gmail.com>
2026-07-30 09:27:27 +0800

    fix: class name fix

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using practice_for_wms.Models.Entities;
+
+namespace practice_for_wms.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {   
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Branch> Branches { get; set; }
+    }
+}
