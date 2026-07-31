4d76d2cc857ab2abebf9b60ab0921b0e86c40178
Clarence0404 <clarencejames.mendoza@citycollegeoftagaytay.edu.ph>
2026-07-31 11:04:42 +0800

    feat: supplier database

--- Controllers/SupplierController.cs
+++ Controllers/SupplierController.cs
@@ -1,17 +1,60 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using practice_for_wms.Data;
+using practice_for_wms.Models;
+using practice_for_wms.Models.Entities;
 
 namespace practice_for_wms.Controllers
 {
     public class SupplierController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public SupplierController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var suppliers = _context.Suppliers.ToList();
+            return View(suppliers);
         }
 
         public IActionResult Add()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Add(string supplierName, string contactPerson, string supplierEmail,
+            string phone, string address)
+        {
+            var supplier = new Supplier
+            {
+                SupplierName = supplierName,
+                ContactPerson = contactPerson ?? "",
+                SupplierEmail = supplierEmail ?? "",
+                Phone = phone ?? "",
+                Address = address ?? "",
+                Status = "Active"
+            };
+            _context.Suppliers.Add(supplier);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            var supplier = _context.Suppliers.Find(id);
+            if (supplier != null)
+            {
+                _context.Suppliers.Remove(supplier);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
         }
     }
 }
