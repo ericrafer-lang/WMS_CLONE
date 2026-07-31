fd2e6bbd9ecd34bf3ae26f4a68137eac371978f5
Clarence0404 <clarencejames.mendoza@citycollegeoftagaytay.edu.ph>
2026-07-31 10:11:59 +0800

    feat: add view product for dashboard

---
+++ Models/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace practice_for_wms.Models.ViewModels
+{
+    public class DashboardViewModel
+    {
+        public int TotalProducts { get; set; }
+        public int TotalSuppliers { get; set; }
+    }
+}
