e0b79f7e5031a90e27aa6deec080d7da1fb3f0fe
Axcel Guevarra <axcelbattleground@gmail.com>
2026-08-04 20:12:46 +0800

    fix: redirect file to folder and add users and branches

---
+++ Models/ViewModels/Dashboard/DashboardViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace practice_for_wms.Models.ViewModels.Dashboard
+{
+    public class DashboardViewModel
+    {
+        public int TotalProducts { get; set; }
+        public int TotalSuppliers { get; set; }
+        public int TotalBranches { get; set; }
+        public int TotalUsers { get; set; }
+    }
+}
