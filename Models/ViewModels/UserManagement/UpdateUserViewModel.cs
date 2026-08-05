fe4f5dd0fb2d4d6c778b07435190754870cfffd9
Axcel Guevarra <axcelbattleground@gmail.com>
2026-08-05 20:54:00 +0800

    WIP: add user update view model

---
+++ Models/ViewModels/UserManagement/UpdateUserViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace practice_for_wms.Models.ViewModels.UserManagement
+{
+    public class UpdateUserViewModel
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; } = string.Empty;
+
+        public string MiddleName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string Role { get; set; } = string.Empty;
+
+        public int BranchId { get; set; }
+    }
+}
