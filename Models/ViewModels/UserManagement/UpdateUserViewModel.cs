80b06ef2c5bf3044ac8526f3e68b80870c5102ab
Axcel Guevarra <axcelbattleground@gmail.com>
2026-08-06 21:41:24 +0800

    feat: initialize update user

--- Models/ViewModels/UserManagement/UpdateUserViewModel.cs
+++ Models/ViewModels/UserManagement/UpdateUserViewModel.cs
@@ -1,19 +1,32 @@
-﻿namespace practice_for_wms.Models.ViewModels.UserManagement
+﻿using System.ComponentModel.DataAnnotations;
+using practice_for_wms.Models.Entities;
+
+namespace practice_for_wms.Models.ViewModels.UserManagement
 {
     public class UpdateUserViewModel
     {
+        [Required]
         public int Id { get; set; }
 
+        [Required]
         public string FirstName { get; set; } = string.Empty;
 
         public string MiddleName { get; set; } = string.Empty;
 
+        [Required]
         public string LastName { get; set; } = string.Empty;
 
+        [Required]
+        [EmailAddress]
         public string Email { get; set; } = string.Empty;
 
+        [Required]
         public string Role { get; set; } = string.Empty;
 
+        [Required]
         public int BranchId { get; set; }
+
+        [Required]
+        public UserStatus Status { get; set; }
     }
 }
