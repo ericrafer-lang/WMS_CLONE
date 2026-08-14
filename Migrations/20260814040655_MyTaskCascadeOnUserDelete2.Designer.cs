b30b940a2a4983f15528a2a492dad3726a6de89d
Clarence0404 <clarencejames.mendoza@citycollegeoftagaytay.edu.ph>
2026-08-14 12:14:12 +0800

    fix: deleting user with task

--- Migrations/20260814040655_MyTaskCascadeOnUserDelete2.Designer.cs
+++ Migrations/20260814040655_MyTaskCascadeOnUserDelete2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using practice_for_wms.Data;
 
@@ -11,9 +12,11 @@ using practice_for_wms.Data;
 namespace practice_for_wms.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260814040655_MyTaskCascadeOnUserDelete2")]
+    partial class MyTaskCascadeOnUserDelete2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
